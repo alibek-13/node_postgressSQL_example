ea9ce2726cd98ff55eddc28d1c49550cc721b58b
unknown <alibekizmagambetov91@gmail.com>
2022-03-22 15:27:55 +0600

    3 commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import userRouter from './router/user.router.js'
 import postRouter from './router/post.router.js'
 
-const PORT =  5000;
+const PORT = process.env.PORT || 5000;
 const app = express()
 
 app.use(cors())
